73298f2c069269f5642e13b2a906472355985627
Cristy g <cristi.gutzu@gmail.com>
2019-08-05 00:54:47 +0200

    map generator

--- map_generator.py
+++ map_generator.py
@@ -1,18 +1,26 @@
 # this script creates the mapping for original file-names
 # to the corrisponding hashes
 
-from os import listdir
+from os import listdir, popen
 from os.path import isfile, join
+import sys
 
+def get_hash(filepath):
+	#ipfs add filepath | cut -f 2 -d " "
+	print(filepath)
+	executed = popen('ipfs add "' + filepath + '" | cut -f2 -d " "').read().split("\n")[0]
+	return (executed)
 
 def list_files(mypath):
-	onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-
-	for static in onlyfiles:
-		print(static)
+	onlyfiles = [mypath+f for f in listdir(mypath) if isfile(join(mypath, f))]
+	return onlyfiles
 
 
 if __name__=="__main__":
-	mypath = "./example/static" #this is the location of the static files you want to publish on CDN
+	mypath = sys.argv[1] #this is the location of the static files you want to publish on CDN
+	files = list_files(mypath)
+	mapping = dict()
+	for file in files:
+		mapping[file] = get_hash(file)
 
-
+	print(mapping)
